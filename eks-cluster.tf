4ae504921af80584d3b949b22ce95052124d3084
devops-tom <thomas2.moran@outlook.com>
2022-03-31 20:16:52 +0100

    remove ingress from tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -4,7 +4,7 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
-  write_kubeconfig = true
+  write_kubeconfig = false
 
   vpc_id = module.vpc.vpc_id
 
